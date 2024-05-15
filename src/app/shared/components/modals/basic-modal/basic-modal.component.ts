37d46b307b711c4f5d3f8d3d7362d1cde02f2533
Json Jimenez <jeisonjimenes.1998@gmail.com>
2024-05-15 17:48:21 -0500

    add service music song

--- src/app/shared/components/modals/basic-modal/basic-modal.component.ts
+++ src/app/shared/components/modals/basic-modal/basic-modal.component.ts
@@ -1,13 +1,20 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-basic-modal',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule
+  ],
   templateUrl: './basic-modal.component.html',
   styleUrl: './basic-modal.component.scss'
 })
 export class BasicModalComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, public dialog: MatDialog) {}
+
+  close() {
+    this.dialog.closeAll();
+  }
 }
