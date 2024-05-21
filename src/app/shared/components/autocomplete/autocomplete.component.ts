d71f1112b1fb966de1459bfd2ac533446ef3d82b
Json Jimenez <jeisonjimenes.1998@gmail.com>
2024-05-21 10:51:31 -0500

    add fix autocomplete bug

--- src/app/shared/components/autocomplete/autocomplete.component.ts
+++ src/app/shared/components/autocomplete/autocomplete.component.ts
@@ -2,12 +2,13 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AsyncPipe, CommonModule} from '@angular/common';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-autocomplete',
   standalone: true,
   imports: [
-    CommonModule, FormsModule, MatAutocompleteModule, ReactiveFormsModule, AsyncPipe
+    CommonModule, FormsModule, MatAutocompleteModule, ReactiveFormsModule, AsyncPipe, MatIconModule,
   ],
   templateUrl: './autocomplete.component.html',
   styleUrl: './autocomplete.component.scss'
